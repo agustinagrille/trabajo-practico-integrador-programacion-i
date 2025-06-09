dbe525f1ccc88b22db61667deb70db0f09d6403e
AraraKu <alexborda92@gmail.com>
2025-06-09 20:33:53 -0300

    actualiacion de comentarios

--- analisis.py
+++ analisis.py
@@ -1,11 +1,10 @@
-def comparar_algoritmos(tiempo_iterativa, tiempo_formula, tiempo_recursiva):
+def comparar_algoritmos(tiempo_iterativa, tiempo_formula, tiempo_recursiva):#Se incia la funcion de comparacion de los algoritmos.
     tiempos = {
         "Iterativa": tiempo_iterativa,
         "Fórmula": tiempo_formula,
         "Recursiva": tiempo_recursiva
     }
     mejor = min(tiempos, key=tiempos.get)
-    print(f"El algoritmo más eficiente fue: {mejor}")
-    for nombre, tiempo in tiempos.items():
-        print(f"- {nombre}: {tiempo:.6f} segundos")
-        
+    print(f"El algoritmo más eficiente fue: {mejor}") #Se imprime el algoritmo de mayor eficiencia.
+    for nombre, tiempo in tiempos.items(): #Inicio del ciclo for
+        print(f"- {nombre}: {tiempo:.6f} segundos")# Se imprime el tiempo de ejeccucion de los algoritmos.
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
-from algoritmos import suma_iterativa, suma_formula, suma_recursiva
+from algoritmos import suma_iterativa, suma_formula, suma_recursiva #Se importan las funciones de los otros modulos.
 from tiempo import medir_tiempo
 from analisis import comparar_algoritmos
 
-def main():
-    n = 10000
+def main():#Nuestra funcion principal
+    n = 10000 #incializo variable con valor 10000
 
     print(f"Calculando la suma de los primeros {n} números...\n")
 
-    tiempo_iterativa, resultado_iterativa = medir_tiempo(suma_iterativa, n)
+    tiempo_iterativa, resultado_iterativa = medir_tiempo(suma_iterativa, n)#Ejecutamos la función suma_iterativa con n, medimos el tiempo que tarda en ejecutarse,
+                                                                           #y guardamos el resultado de la suma y el tiempo en dos variables
     print(f"Suma iterativa: Resultado = {resultado_iterativa}, Tiempo = {tiempo_iterativa:.6f} segundos")
 
-    tiempo_formula, resultado_formula = medir_tiempo(suma_formula, n)
+    tiempo_formula, resultado_formula = medir_tiempo(suma_formula, n)#Medimos cuánto tarda suma_formula(n) y guardamos el tiempo y el resultado
     print(f"Suma con fórmula: Resultado = {resultado_formula}, Tiempo = {tiempo_formula:.6f} segundos")
 
-    tiempo_recursiva, resultado_recursiva = medir_tiempo(suma_recursiva, n)
+    tiempo_recursiva, resultado_recursiva = medir_tiempo(suma_recursiva, n)#Medimos cuánto tarda suma_recursiva(n) y guardamos el tiempo y el resultado
     print(f"Suma recursiva: Resultado = {resultado_recursiva}, Tiempo = {tiempo_recursiva:.6f} segundos")
 
     print("\nAnálisis de eficiencia:")
-    comparar_algoritmos(tiempo_iterativa, tiempo_formula, tiempo_recursiva)
+    comparar_algoritmos(tiempo_iterativa, tiempo_formula, tiempo_recursiva)#Comparamos los resultados de los tiempos de cada algoritmos
 
-if name == "main":
+if __name__ == "__main__":
     main()
